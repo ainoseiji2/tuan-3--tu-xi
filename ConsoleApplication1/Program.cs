84263ab3d686d3adebce1847769d35d602086aaa
khang <sagasggas@gmail.com>
2018-03-20 18:37:16 +0700

    oan tu xi

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipend = new IPEndPoint(IPAddress.Any, 9050);
+            newsock.Bind(ipend);
+            EndPoint ep = ipend;
+            while (true)
+            {
+                byte[] receivedata = new byte[20];
+                newsock.ReceiveFrom(receivedata, ref ep);
+                int result = Convert.ToInt32(Encoding.ASCII.GetString(receivedata));
+                {
+                    Random r = new Random();
+                    int serverresult = r.Next(0, 3);
+                    if (serverresult == 0 && result == 1 || serverresult == 1 && result == 2 || serverresult == 2 && result == 1)
+                    {
+                        byte[] sendData = Encoding.ASCII.GetBytes("Thua");
+                        newsock.SendTo(sendData, 0, ep);
+                    }
+                    else if (serverresult == 0 && result == 2 || serverresult == 1 && result == 0 || serverresult == 2 && result == 1)
+                    {
+                        byte[] sendData = Encoding.ASCII.GetBytes("Thang");
+                        newsock.SendTo(sendData, 0, ep);
+                    }
+                    else
+                    {
+                        byte[] sendData = Encoding.ASCII.GetBytes("Hoa");
+                        newsock.SendTo(sendData, 0, ep);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tuxi/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+
+namespace Tuxi
+{
+    public partial class Form1 : Form
+    {
+        Socket server, client;
+        EndPoint remote;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint gui = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            remote = (EndPoint)gui;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bua";
+            byte[] sendData = Encoding.ASCII.GetBytes("0");
+            server.SendTo(sendData, remote);
+            byte[] receivedata = new byte[20];
+            server.ReceiveFrom(receivedata, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(receivedata);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Keo";
+            byte[] sendData = Encoding.ASCII.GetBytes("1");
+            server.SendTo(sendData, remote);
+            byte[] receivedata = new byte[20];
+            server.ReceiveFrom(receivedata, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(receivedata);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bao";
+            byte[] sendData = Encoding.ASCII.GetBytes("2");
+            server.SendTo(sendData, remote);
+            byte[] receivedata = new byte[20];
+            server.ReceiveFrom(receivedata, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(receivedata);
+        }
+    }
+}
